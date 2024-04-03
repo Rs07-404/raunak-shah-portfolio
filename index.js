57f1a02eed340441ceba71cf2990fc97ac62a176
Raunak Shah <shahrrs2004@gmail.com>
2024-04-03 22:41:24 +0530

    Added animations

--- index.js
+++ index.js
@@ -17,3 +17,20 @@ menu.addEventListener('click',(event) =>{
     }
     console.log("clicked "+event+menu)
 })
+
+window.addEventListener("scroll", function(){
+    var cards = document.querySelectorAll(".card");
+    for(let  i = 0; i < cards.length; i ++){
+        let windowHeight = window.innerHeight;
+        let element = cards[i].getBoundingClientRect();
+        if( element.top >= 0 && 
+            element.left >= 0 && 
+            element.bottom <= (windowHeight || document.documentElement.clientHeight) && 
+            element.right <= (windowHeight || document.documentElement.clientWidth)
+            ){
+            cards[i].classList.add("active");
+        }else{
+            cards[i].classList.remove("active");
+        }
+    }
+})
